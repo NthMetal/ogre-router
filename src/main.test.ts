8ba170f57ed109d205f9199d5b03b056e485b164
nthmetal <832529@gmail.com>
2021-10-03 23:41:29 -0400

    fix docs and tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,10 +1,10 @@
-import { 
-  Ogre,
-  Predecessor,
-  Signaler,
-  Successor,
-  User
-} from './main'
+// import { 
+//   Ogre,
+//   Predecessor,
+//   Signaler,
+//   Successor,
+//   User
+// } from './main'
 
 test('the data is peanut butter', () => {
   expect(1).toBe(1)
